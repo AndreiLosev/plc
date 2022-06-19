57fcff369add06e18db48c877ad7df59040caa41
Andrei <losev_andrei@bk.ru>
2022-06-19 18:26:26 +0300

    add modbus tcp server

--- src/system_prog/modbus_rtu_server.rs
+++ src/system_prog/modbus_rtu_server.rs
@@ -1,72 +1,82 @@
-// use std::io::{Read, Write};
-// use super::super::task::Program;
-// use rmodbus::server::context::ModbusContext;
-// use rmodbus::server::ModbusFrame;
-// use rmodbus::ModbusProto;
-// use rmodbus::ModbusFrameBuf;
-// use std::{result, error, io};
-// use ansi_term::Color::Red;
+use std::io::{Read, Write};
+use std::{result, error, io, time::Duration};
+use std::cell::RefCell;
 
-// pub struct ModbusTcpServer {
-//     id: u8,
-//     listener: serial::SystemPort,
-// }
+use super::super::task::ConstProgram;
+use rmodbus::server::context::ModbusContext;
+use rmodbus::server::ModbusFrame;
+use rmodbus::ModbusProto;
+use rmodbus::ModbusFrameBuf;
+use serial::SerialPort;
+pub use serial::PortSettings;
+pub use serial::{BaudRate, Parity, CharSize, StopBits, FlowControl};
+use ansi_term::Color::Red;
 
-// impl ModbusTcpServer {
+pub struct ModbusRtuServer {
+    id: u8,
+    port: RefCell<serial::SystemPort>,
+}
 
-//     pub fn new(id: u8, listen: &'static str) -> Self {
+impl ModbusRtuServer {
 
-//         let listener = Self::create_listener(listen);
+    pub fn new(id: u8, listen: &'static str, settings: serial::PortSettings) -> Self {
 
-//         Self { id, listener}
-//     }
+        let port = Self::create_prot(listen, settings);
 
-//     fn create_listener(listen: &'static str) -> TcpListener {
-//         let listener = TcpListener::bind(listen)
-//         .unwrap_or_else(|e| panic!(
-//             "err: {}",
-//             &Red.paint(format!("{}", e))
-//         ));
-//         listener.set_nonblocking(true)
-//             .unwrap_or_else(|e| panic!(
-//                 "err: {}",
-//                 &Red.paint(format!("{}", e))
-//             ));
+        Self { id, port: RefCell::new(port) }
+    }
 
-//         listener
-//     }
-// }
+    fn create_prot(listen: &'static str, settings: serial::PortSettings) -> serial::SystemPort {
+        let mut port = serial::open(listen)
+            .unwrap_or_else(|e| panic!(
+                "err: {}",
+                &Red.paint(format!("{}", e))
+            )); 
+
+        port.configure(&settings)
+            .unwrap_or_else(|e| panic!(
+                "err: {}",
+                &Red.paint(format!("{}", e))
+            )); 
+
+        port.set_timeout(Duration::ZERO)
+            .unwrap_or_else(|e| panic!(
+                "err: {}",
+                &Red.paint(format!("{}", e))
+            )); 
+
+        port
+    }
+}
 
 
-// impl<'a> Program for ModbusTcpServer {
-//     fn run(&mut self, context: &mut ModbusContext) -> result::Result<(), Box<dyn error::Error>> {
+impl<'a> ConstProgram for ModbusRtuServer {
+    fn run(&self, context: &mut ModbusContext) -> result::Result<(), Box<dyn error::Error>> {
         
-//         let mut stream = match self.listener.accept() {
-//             Ok((stream, _)) => stream,
-//             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => return Ok(()),
-//             Err(e) => return Err(Box::new(e)),
-//         };
-
-//         loop {
+        loop {
          
-//             let mut buf: ModbusFrameBuf = [0; 256];
-//             let mut response: Vec<u8> = Vec::with_capacity(8);
-//             stream.read(&mut buf)?;
-//             let mut frame = ModbusFrame::new(self.id, &buf, ModbusProto::TcpUdp, &mut response);
-//             frame.parse()?;
+            let mut buf: ModbusFrameBuf = [0; 256];
+            let mut response: Vec<u8> = Vec::with_capacity(8);
+            match self.port.borrow_mut().read(&mut buf) {
+                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => return Ok(()),
+                Err(e) => return Err(Box::new(e)),
+                Ok(_) => (),
+            };
+            let mut frame = ModbusFrame::new(self.id, &buf, ModbusProto::Rtu, &mut response);
+            frame.parse()?;
     
-//             if frame.processing_required {
-//                 match frame.readonly {
-//                     true => frame.process_read(context),
-//                     false => frame.process_write(context),
-//                 }?;
-//             }
+            if frame.processing_required {
+                match frame.readonly {
+                    true => frame.process_read(context),
+                    false => frame.process_write(context),
+                }?;
+            }
     
-//             if frame.response_required {
-//                 frame.finalize_response()?;
-//                 stream.write(response.as_slice())?;
-//                 break Ok(());
-//             }          
-//         }      
-//     }
-// }
+            if frame.response_required {
+                frame.finalize_response()?;
+                self.port.borrow_mut().write(response.as_slice())?;
+                break Ok(());
+            }          
+        }
+    }
+}
